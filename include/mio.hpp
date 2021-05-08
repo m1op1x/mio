51da301694e56e9553ded030ccc853600d05f6fd
m1op1x <sbh.303@gmail.com>
2021-05-08 18:17:40 +0930

    Library include-all header file

---
+++ include/mio.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <mio/circle.hpp>
+#include <mio/clock.hpp>
+#include <mio/color.hpp>
+#include <mio/context.hpp>
+#include <mio/event_types.hpp>
+#include <mio/event.hpp>
+#include <mio/exception.hpp>
+#include <mio/font.hpp>
+#include <mio/gamepad_axis.hpp>
+#include <mio/gamepad_button.hpp>
+#include <mio/gamepad.hpp>
+#include <mio/image.hpp>
+#include <mio/keyboard.hpp>
+#include <mio/math.hpp>
+#include <mio/mouse_button.hpp>
+#include <mio/mouse.hpp>
+#include <mio/point.hpp>
+#include <mio/random.hpp>
+#include <mio/rectangle.hpp>
+#include <mio/renderer.hpp>
+#include <mio/scancode.hpp>
+#include <mio/texture_access.hpp>
+#include <mio/texture.hpp>
+#include <mio/window.hpp>
