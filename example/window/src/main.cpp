9ea440a5942085542deba16565e097a2f13102c6
m1op1x <sbh.303@gmail.com>
2021-05-10 23:52:49 +0930

    Window - "Hello, World!" style example project

---
+++ example/window/src/main.cpp
@@ -0,0 +1,27 @@
+#include <mio.hpp>
+
+int main()
+{
+    mio::context context;
+
+    mio::window window { "title", 1280, 720 };
+
+    mio::renderer renderer { window };
+
+    bool is_running = true;
+
+    while (is_running)
+    {
+        while (auto event = mio::event::poll())
+        {
+            if (event->is<mio::quit_event>())
+            {
+                is_running = false;
+            }
+        }
+
+        renderer.clear(mio::color::white);
+
+        renderer.present();
+    }
+}
