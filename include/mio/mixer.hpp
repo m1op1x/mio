6fdc8dca382acc1a9f7adb1d04041a917d972f2a
scott <sbh.303@gmail.com>
2021-05-15 19:13:53 +0930

    Mixer - Initial support for sound playback

--- include/mio/mixer.hpp
+++ include/mio/mixer.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
 #include <mio/music.hpp>
+#include <mio/sound.hpp>
 
 namespace mio::mixer
 {
+    void play_sound(const sound& sound, int channel);
+
+    int play_sound(const sound& sound);
+
+    void pause_sound(int channel);
+
+    void resume_sound(int channel);
+
+    void stop_sound(int channel);
+
     void play_music(const music& music);
 
     void pause_music();
--- src/mixer.cpp
+++ src/mixer.cpp
@@ -5,6 +5,39 @@
 
 namespace mio::mixer
 {
+    void play_sound(const sound& sound, int channel)
+    {
+        if (Mix_PlayChannel(channel, sound.native_handle(), 0) == -1)
+        {
+            throw mix_error();
+        }
+    }
+
+    int play_sound(const sound& sound)
+    {
+        int channel = Mix_PlayChannel(-1, sound.native_handle(), 0);
+        if (channel == -1)
+        {
+            throw mix_error();
+        }
+        return channel;
+    }
+
+    void pause_sound(int channel)
+    {
+        Mix_Pause(channel);
+    }
+
+    void resume_sound(int channel)
+    {
+        Mix_Resume(channel);
+    }
+
+    void stop_sound(int channel)
+    {
+        Mix_HaltChannel(channel);
+    }
+
     void play_music(const music& music)
     {
         if (Mix_PlayMusic(music.native_handle(), 1))
